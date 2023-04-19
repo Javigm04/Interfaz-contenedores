ab6d83195ebae1bd28e28549dfe6655ed172b9bb
Alex <alex.ortega@alu.uclm.es>
2023-04-19 19:01:16 +0200

    commit interfaz terminada

--- src/PaqG04/Contenedores.java
+++ src/PaqG04/Contenedores.java
@@ -1,16 +1,12 @@
 package PaqG04;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 public class Contenedores extends JFrame{
-    private JButton button17;
-    private JButton button19;
-    private JButton button20;
-    private JButton button18;
+    private JButton mapaButton;
+    private JButton desapilarButton;
+    private JButton mostrarDatosButton;
+    private JButton apilarButton;
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
@@ -21,6 +17,7 @@ public class Contenedores extends JFrame{
     private JTextField textField8;
     private JTextField texto2;
     private JPanel Contenedores;
+    private JButton contenedorPorPaisButton;
 
     Contenedores(){
         setTitle("Contenedores");
