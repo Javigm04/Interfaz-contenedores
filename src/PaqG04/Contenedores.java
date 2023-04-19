28be189ef8f41330daa02bd5b0c85a3e9433dbd1
Alex <alex.ortega@alu.uclm.es>
2023-04-19 18:29:44 +0200

    commit prueba

--- src/PaqG04/Contenedores.java
+++ src/PaqG04/Contenedores.java
@@ -7,8 +7,14 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Contenedores extends JFrame{
+    private JButton boton1;
+    private JButton boton2;
+    private JTextField texto1;
+    private JTextField texto2;
+    private JLabel titulo;
     private JPanel Contenedores;
-
+    private JLabel Contenedor;
+    private JButton apilarButton;
 
     Contenedores(){
         setTitle("Contenedores");
@@ -16,11 +22,21 @@ public class Contenedores extends JFrame{
         setDefaultCloseOperation(JInternalFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setContentPane(Contenedores);
+        boton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        apilarButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                System.out.println("Me has clikado");
+            }
+        });
     }
-
-
     public static void main(String[] args) {
         Contenedores contenedores=new Contenedores();
-
     }
 }
