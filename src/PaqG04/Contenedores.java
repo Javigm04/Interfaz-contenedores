231c41680c1a7dffe20bbc7832bf62f34bee1b7f
Usuario <Javier.Garcia127@alu.uclm.es>
2023-04-19 19:42:11 +0200

    Commit interface

--- src/PaqG04/Contenedores.java
+++ src/PaqG04/Contenedores.java
@@ -1,31 +1,59 @@
 package PaqG04;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Contenedores extends JFrame{
     private JButton mapaButton;
     private JButton desapilarButton;
     private JButton mostrarDatosButton;
     private JButton apilarButton;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JTextField textField6;
-    private JTextField textField7;
+    private JTextField id;
+    private JTextField peso;
+    private JTextField prioridad;
+    private JTextField resultados;
+    private JTextField descripcion;
+    private JTextField empresaenvia;
+    private JTextField empresarecibe;
     private JTextField textField8;
     private JTextField texto2;
     private JPanel Contenedores;
     private JButton contenedorPorPaisButton;
+    private JTextField pais;
+    private JTextField aduana;
+    private JTextPane textPane1;
 
     Contenedores(){
         setTitle("Contenedores");
-        setSize(400,300);
+        setSize(1000,700);
         setDefaultCloseOperation(JInternalFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setContentPane(Contenedores);
+        Hub Valencia=new Hub(10,12);
 
+        apilarButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+        });
+        mapaButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textPane1.setText(Valencia.toString());
+            }
+        });
+        mostrarDatosButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+        });
+        contenedorPorPaisButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textPane1.setText(Valencia.calcularContenedoresDeterminadoPais(pais.getText()));
+            }
+        });
     }
     public static void main(String[] args) {
         Contenedores contenedores=new Contenedores();
